4093a9696d5d922dbdb2318751d3b0ae3c939975
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2021-11-06 02:54:58 +0100

    Created Customers and Loans Schema

---
+++ models/loans.model.ts
@@ -0,0 +1,11 @@
+const loanSchema = mongoose.Schema({
+    loanName: String,
+    loanType: String,
+    loanAmount: Number,
+    loanIssueDate: Date,
+    loanStatus: String
+});
+
+const loanModel = mongoose.model('Loans', loanSchema);
+
+module.exports = loanModel;
